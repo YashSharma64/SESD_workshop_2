e91e4e3997a390003d502278cc3cfadd9c030310
Yash Sharma <yashsharma.aiml@gmail.com>
2026-03-04 00:50:58 +0530

    Wire calc, uuid, fileinfo, helpme commands into CLI

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,18 @@ import chalk from "chalk";
 import { GreetCommand } from "./commands/GreetCommand";
 import { TimeCommand } from "./commands/TimeCommand";
 import { RandomCommand } from "./commands/RandomCommand";
+import { CalcCommand } from "./commands/CalcCommand";
+import { UuidCommand } from "./commands/UuidCommand";
+import { FileInfoCommand } from "./commands/FileInfoCommand";
+import { HelpMeCommand } from "./commands/HelpMeCommand";
 
 const greet = new GreetCommand();
 const time = new TimeCommand();
 const random = new RandomCommand();
+const calc = new CalcCommand();
+const uuid = new UuidCommand();
+const fileinfo = new FileInfoCommand();
+const helpme = new HelpMeCommand();
 
 const program = new Command();
 
@@ -31,9 +39,33 @@ program
   .description(random.description)
   .action(() => random.run());
 
+program
+  .command(calc.name)
+  .description(calc.description)
+  .argument("<num1>", "First number")
+  .argument("<operator>", "Operator (+, -, *, /)")
+  .argument("<num2>", "Second number")
+  .action((a, op, b) => calc.run([a, op, b]));
+
+program
+  .command(uuid.name)
+  .description(uuid.description)
+  .action(() => uuid.run());
+
+program
+  .command(fileinfo.name)
+  .description(fileinfo.description)
+  .argument("<filename>", "File to inspect")
+  .action((file) => fileinfo.run([file]));
+
+program
+  .command(helpme.name)
+  .description(helpme.description)
+  .action(() => helpme.run());
+
 program.addHelpText(
   "after",
-  `\n${chalk.gray("Phase 2: Basic commands (greet, time, random) implemented.")}`
+  `\n${chalk.gray("Phase 3: All local commands implemented. API commands next.")}`
 );
 
 if (process.argv.length <= 2) {
